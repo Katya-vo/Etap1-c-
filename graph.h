2a22b9b7589c2b435c7847ce620154171d305bb5
Katya-vo <01205792@pw.edu.pl>
2026-03-09 01:52:44 +0200

    Update and rename stats.h to graph.h

---
+++ graph.h
@@ -0,0 +1,12 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+typedef struct {
+    int u;
+    int v;
+} Edge;
+
+int read_graph(const char *filename, Edge **edges);
+
+#endif
+
--- stats.h
+++
@@ -1,8 +0,0 @@
-#ifndef STATS_H
-#define STATS_H
-
-int minimum(int *arr, int n);
-int maximum(int *arr, int n);
-double average(int *arr, int n);
-
-#endif
