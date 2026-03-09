86b69c26ab85d8829ed5a2c1dbef67fb47f810f5
jagnaszapiel <01206827@pw.edu.pl>
2026-03-09 01:06:52 +0100

    Update layout.c

--- layout.c
+++ layout.c
@@ -1,6 +1,15 @@
 #include <math.h>
 #include "layout.h"
 
-void circle_layout(double *x, double *y, int n) {}
+void circle_layout(double *x, double *y, int n) {
+  double R = 10.0;
+
+    for (int i = 0; i < n; i++)
+    {
+        x[i] = R * cos(2 * M_PI * i / n);
+        y[i] = R * sin(2 * M_PI * i / n);
+    }
+}
+
 
 
